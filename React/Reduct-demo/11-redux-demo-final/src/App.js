64d221b86a2cf36be8df4798bfbb8700736b5442
fortrainees2020 <64248056+fortrainees2020@users.noreply.github.com>
2024-03-19 10:33:03 +0530

    Add files via upload

---
+++ React/Reduct-demo/11-redux-demo-final/src/App.js
@@ -0,0 +1,15 @@
+import Purchase from './components/Purchase';
+import Cart from './components/Cart';
+import Total from './components/Total';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Purchase/>
+      <Cart/>
+      <Total/>
+    </div>
+  );
+}
+export default App;
